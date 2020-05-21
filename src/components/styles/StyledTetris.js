0f7fbd5771e202b59f74aed6cc768ae34c161681
K Smith <klinton.jp@gmail.com>
2020-05-20 20:42:19 -0600

    custom hooks, stage update and player movement

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -4,7 +4,7 @@ import bgImage from "../../img/bg.png";
 import Display from "../Display";
 
 export const StyledTetrisWrapper = styled.div`
-  width: 100vh;
+  width: 100vw;
   height: 100vh;
   background: url(${bgImage}) #000;
   background-size: cover;
